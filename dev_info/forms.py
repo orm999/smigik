f4149b40fa660318ac10c0bd1344682b924804ff
lynx_r <ya@lynx-r.name>
2011-01-19 15:36:43 +0300

    radio button.

--- dev_info/forms.py
+++ dev_info/forms.py
@@ -1,13 +1,14 @@
 # -*- coding: utf -*-
 from django import forms
 
-class InputDevInfoForm( forms.Form ):
+class InputDevEditForm( forms.Form ):
     model = forms.CharField( max_length=255, label='Модель' )
     expl_start_date = forms.DateField( label='Дата начала эксплуатации' )
     scan_mode = forms.CharField( max_length=255, label='Режим сканирования' )
 
-class OutputDevInfoForm( forms.Form ):
+class OutputDevEditForm( forms.Form ):
     model = forms.CharField( max_length=255, label='Модель' )
     expl_start_date = forms.DateField( label='Дата начала эксплуатации' )
     cartridge_id = forms.IntegerField( label='Номер картриджа' )
     print_mode = forms.CharField( max_length=255, label='Режим печати' )
+
